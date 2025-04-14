1276aecb3759aa7cef4008f54f6bac79d6fd0877
CarloGtz <carmargurmao@gmail.com>
2025-04-13 20:02:42 -0600

    Validar formularios, purificar codigos de cartas, cambiar class ahora con popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,37 @@
 const Name = document.querySelector(".user").querySelector(".name");
 const topic = document.querySelector(".user").querySelector(".topic");
 
-const userEdit = document.querySelector(".edit-button");
-const addCard = document.querySelector(".add-button");
+const userEdit = document.querySelector(".edit-popup-button");
+const addCard = document.querySelector(".add-popup-button");
 
-const popUpEdit = document.querySelector(".edit");
-const popUpAdd = document.querySelector(".add");
+const popUpEdit = document.querySelector(".edit-popup");
+const popUpAdd = document.querySelector(".add-popup");
 
-const formEdit = popUpEdit.querySelector(".edit__form");
-const formAdd = popUpAdd.querySelector(".add__form");
+const editCover = popUpEdit.querySelector(".edit-popup__cover");
+const addCover = popUpAdd.querySelector(".add-popup__cover");
+
+const formEdit = popUpEdit.querySelector(".edit-popup__form");
+const formAdd = popUpAdd.querySelector(".add-popup__form");
 
 const closeEdit = popUpEdit.querySelector(".close");
-const closeAdd = popUpAdd.querySelector(".add__close");
+const closeAdd = popUpAdd.querySelector(".add-popup__close");
 
-const editInputs = popUpEdit.querySelectorAll(".edit__input");
-const editInputNombre = popUpEdit.querySelector(".edit__input-nombre");
-const editInputAcerca = popUpEdit.querySelector(".edit__input-acerca");
+const editInputs = popUpEdit.querySelectorAll(".edit-popup__input");
+const editInputNombre = popUpEdit.querySelector(".edit-popup__input_nombre");
+const editInputAcerca = popUpEdit.querySelector(".edit-popup__input_acerca");
 
-const addInputs = popUpAdd.querySelectorAll(".add__input");
-const addInputTitulo = popUpAdd.querySelector(".add__input-titulo");
-const addInputImagen = popUpAdd.querySelector(".add__input-imagen");
+const addInputs = popUpAdd.querySelectorAll(".add-popup__input");
+const addInputTitulo = popUpAdd.querySelector(".add-popup__input_titulo");
+const addInputImagen = popUpAdd.querySelector(".add-popup__input_imagen");
 
-const editButton = document.getElementById("edit__button-save");
-const addButton = document.getElementById("add__button-save");
+const editButton = document.getElementById("edit-popup__button-save");
+const addButton = document.querySelector(".add-popup__button-save");
+
+const editSpanNombre = document.querySelector(".edit-popup__span_nombre");
+const editSpanAcerca = document.querySelector(".edit-popup__span_acerca");
+
+const addSpanTitulo = document.querySelector(".add-popup__span_titulo");
+const addSpanImagen = document.querySelector(".add-popup__span_imagen");
 
 const cardsContainer = document.getElementById("cards");
 const cards = document.querySelectorAll(".card");
@@ -36,35 +45,43 @@ const storageCards = [
   {
     name: "Eduard Delputte",
     link: "https://images.unsplash.com/photo-1740137660661-274c804a891d?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    like: null,
   },
   {
     name: "Ciudad del cabo",
     link: "https://images.unsplash.com/photo-1740021546242-8b718a3e0459?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    like: null,
   },
   {
     name: "Pierpaolo Pellegrino",
     link: "https://images.unsplash.com/photo-1739775225955-ba1ba496d28d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    like: null,
   },
   {
     name: "Nebulosa",
     link: "https://images.unsplash.com/photo-1740094714220-1b0c181be46d?q=80&w=2083&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    like: null,
   },
   {
     name: "Parque Nacional de Yosemite",
     link: "https://images.unsplash.com/photo-1739993655680-4b7050ed2896?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    like: null,
   },
   {
     name: "Isla Guadalupe",
     link: "https://images.unsplash.com/photo-1740175919285-451699588f1b?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    like: null,
   },
 ];
 
 class AddCards {
-  constructor(titulo, imagen, id) {
+  constructor(titulo, imagen, id, likeComprovation) {
     this.element = cardTemplate.cloneNode(true).firstElementChild;
     const cardName = this.element.querySelector(".card__name");
     const cardImage = this.element.querySelector(".card__image");
     const eliminador = this.element.querySelector(".icono-basura");
+    const like = this.element.querySelector(".card__info-like");
+    this.likingCard(like, likeComprovation);
 
     cardName.textContent = titulo;
     cardImage.src = imagen;
@@ -76,6 +93,16 @@ class AddCards {
 
     cardImage.addEventListener("click", () => {
       this.createGrandImage(titulo, imagen);
+    });
+
+    like.addEventListener("click", () => {
+      if (!likeComprovation) {
+        likeComprovation = true;
+      } else {
+        likeComprovation = false;
+      }
+      this.comfirmatedCard(titulo, likeComprovation);
+      this.likingCard(like, likeComprovation);
     });
 
     cardsContainer.prepend(this.element);
@@ -108,8 +135,8 @@ class AddCards {
 
   renderCards() {
     cardsContainer.innerHTML = "";
-    recorrerArreglo(storageCards, (card) => {
-      new AddCards(card.name, card.link);
+    recorrerArreglo(storageCards, (card, index) => {
+      new AddCards(card.name, card.link, index, card.like);
     });
   }
 
@@ -117,133 +144,135 @@ class AddCards {
     storageCards.splice(id, 1);
   }
 
-  likingCard() {}
+  likingCard(imgElement, likeComprovation) {
+    if (!likeComprovation) {
+      imgElement.style.height = "";
+      imgElement.style.paddingTop = "";
+      return (imgElement.src = "./images/Corazon.svg");
+    } else {
+      imgElement.style.height = "19px";
+      imgElement.style.paddingTop = "5px";
+      return (imgElement.src = "./images/CorazonActive.png");
+    }
+  }
+  comfirmatedCard(title, like) {
+    storageCards.map((elemento) => {
+      if (elemento.name === title) {
+        return (elemento.like = like);
+      }
+    });
+  }
 }
 
 function formEditVisibility() {
-  popUpEdit.classList.toggle("visibility__form");
+  popUpEdit.classList.toggle("edit-popup__visibility__form");
   editInputNombre.placeholder = Name.textContent;
   editInputNombre.value = Name.textContent;
   editInputAcerca.placeholder = topic.textContent;
   editInputAcerca.value = topic.textContent;
+  validity(editInputNombre, editSpanNombre);
+  validity(editInputAcerca, editSpanAcerca);
 }
 
 function formAddVisibility() {
-  popUpAdd.classList.toggle("add__visibility__form");
-}
-
-function toggleEditButton() {
-  const allFilledEdit = Array.from(editInputs).every(
-    (input) => input.value.trim() !== ""
-  );
-
-  if (allFilledEdit) {
-    editButton.removeAttribute("disabled");
-    editButton.classList.add("edit__button-save-active");
-  } else {
-    editButton.setAttribute("disabled", "true");
-    editButton.classList.remove("edit__button-save-active");
-  }
-}
-
-function toggleAddButton() {
-  const allFilledAdd = Array.from(addInputs).every(
-    (input) => input.value.trim() !== ""
-  );
-
-  if (allFilledAdd) {
-    addButton.removeAttribute("disabled");
-    addButton.classList.add("add__button-save-active");
-  } else {
-    addButton.setAttribute("disabled", "true");
-    addButton.classList.remove("add__button-save-active");
-  }
-}
-
-function cardActionLike(imgElement) {
-  const currentSrc = imgElement.getAttribute("src");
-  const newSrc =
-    currentSrc === "./images/Corazon.svg"
-      ? "./images/CorazonActive.png"
-      : "./images/Corazon.svg";
-  imgElement.setAttribute("src", newSrc);
-
-  if (newSrc === "./images/CorazonActive.png") {
-    imgElement.style.height = "21px";
-    imgElement.style.paddingTop = "5px";
-  } else {
-    imgElement.style.height = "";
-    imgElement.style.paddingTop = "";
-  }
+  popUpAdd.classList.toggle("add-popup__visibility__form");
 }
 
 function renovarValoresusuario() {
   const nuevoNombreValor = editInputNombre.value.trim();
   const nuevoAcercaValor = editInputAcerca.value.trim();
-  if (nuevoNombreValor && nuevoAcercaValor) {
+  if (validity) {
     topic.textContent = nuevoAcercaValor;
     Name.textContent = nuevoNombreValor;
     formEditVisibility();
   }
 }
 
-function addCardAction() {
-  const titulo = addInputTitulo.value.trim();
-  const imagen = addInputImagen.value.trim();
+function addCardAction(title, image) {
+  const titulo = title.value.trim();
+  const imagen = image.value.trim();
 
   if (titulo && imagen) {
-    formAddVisibility();
-    /* new AddCards(titulo, imagen); */
     storageCards.push({ name: titulo, link: imagen });
     cardsContainer.innerHTML = "";
 
     recorrerArreglo(storageCards, (card, index) => {
-      new AddCards(card.name, card.link, index);
+      new AddCards(card.name, card.link, index, card.like);
     });
 
     addInputTitulo.value = "";
     addInputImagen.value = "";
+    formAddVisibility();
   }
 }
 
 function recorrerArreglo(array, callback) {
-  array.forEach(callback /* (item, index) => callback(item, index) */);
+  array.forEach((item, index) => callback(item, index));
+}
+
+function popUpEsc(popUp, toggle, event, section) {
+  if (event.key === "Escape") {
+    if (popUp.classList.contains(`${section}-popup__visibility__form`)) {
+      toggle();
+    }
+  }
 }
 
 recorrerArreglo(storageCards, (card, index) => {
-  new AddCards(card.name, card.link, index);
+  new AddCards(card.name, card.link, index, card.like);
 });
 
 editInputs.forEach((input) => {
-  input.addEventListener("input", toggleEditButton);
+  input.addEventListener("input", () => {
+    buttonToggle(
+      validity(editInputNombre, editSpanNombre),
+      validity(editInputAcerca, editSpanAcerca),
+      editButton,
+      "edit"
+    );
+  });
 });
 
 addInputs.forEach((input) => {
-  input.addEventListener("input", toggleAddButton);
+  input.addEventListener("input", () => {
+    buttonToggle(
+      validity(addInputTitulo, addSpanTitulo),
+      validity(addInputImagen, addSpanImagen),
+      addButton,
+      "add"
+    );
+  });
 });
 
 userEdit.addEventListener("click", formEditVisibility);
 closeEdit.addEventListener("click", formEditVisibility);
+editCover.addEventListener("click", formEditVisibility);
+document.addEventListener("keydown", (evt) => {
+  popUpEsc(popUpEdit, formEditVisibility, evt, "edit");
+});
 
 addCard.addEventListener("click", formAddVisibility);
 closeAdd.addEventListener("click", formAddVisibility);
+addCover.addEventListener("click", formAddVisibility);
+document.addEventListener("keydown", (evt) => {
+  popUpEsc(popUpAdd, formAddVisibility, evt, "add");
+});
 
-document
-  .querySelector(".add__form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita el envío tradicional
-    addCardAction();
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  addCardAction(addInputTitulo, addInputImagen);
+});
+
+addInputs.forEach((input) => {
+  input.addEventListener("keydown", (evt) => {
+    if (evt.key === "Enter") {
+      evt.preventDefault();
+      addCardAction(addInputTitulo, addInputImagen);
+    }
   });
+});
 
-document
-  .querySelector(".edit__form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita el envío tradicional
-    renovarValoresusuario();
-  });
-
-cardsContainer.addEventListener("click", (event) => {
-  if (event.target.classList.contains("card__info-like")) {
-    cardActionLike(event.target);
-  }
+formEdit.addEventListener("submit", (event) => {
+  event.preventDefault();
+  renovarValoresusuario();
 });
---
+++ scripts/validate.js
@@ -0,0 +1,112 @@
+const ValidationElements = {
+  edit: {
+    formSelector: ".edit-popup__form",
+    contenedorSelector: ".edit-popup__form-contenedor",
+    inputSelector: ".edit-popup__input",
+    submitButtonSelector: ".edit-popup__button-save",
+    inactiveButtonClass: "edit-popup__button-save-unactive",
+    inputErrorClass: "edit-popup__input_type_error",
+    errorSpanClass: "edit-popup__span",
+  },
+  add: {
+    formSelector: ".add-popup__form",
+    contenedorSelector: ".add-popup__form-contenedor",
+    inputSelector: ".add-popup__input",
+    submitButtonSelector: ".add-popup__button-save",
+    inactiveButtonClass: "add-popup__button-save-unactive",
+    inputErrorClass: "add-popup__input_type_error",
+    errorSpanClass: "add-popup__span",
+  },
+};
+
+const enableValidation = (input, span, typeError) => {
+  if (!input.enableValidation.valid) {
+    span.innerText = input.validationMessage;
+    input.classList.add(typeError);
+    return false;
+  } else {
+    span.innerText = "";
+    input.classList.remove(typeError);
+    return true;
+  }
+};
+
+const buttonToggle = (one, two, button, unactive) => {
+  if (one && two) {
+    button.removeAttribute("disabled");
+    button.classList.add(unactive);
+  } else {
+    button.setAttribute("disabled", "true");
+    button.classList.remove(unactive);
+  }
+};
+
+editInputNombre.addEventListener("input", () => {
+  enableValidation(
+    editInputNombre,
+    editSpanNombre,
+    ValidationElements.edit.inputErrorClass
+  );
+});
+
+editInputAcerca.addEventListener("input", () => {
+  enableValidation(
+    editInputAcerca,
+    editSpanAcerca,
+    ValidationElements.edit.inputErrorClass
+  );
+});
+
+addInputTitulo.addEventListener("input", () => {
+  enableValidation(
+    addInputTitulo,
+    addSpanTitulo,
+    ValidationElements.add.inputErrorClass
+  );
+});
+
+addInputImagen.addEventListener("input", () => {
+  enableValidation(
+    addInputImagen,
+    addSpanImagen,
+    ValidationElements.add.inputErrorClass
+  );
+});
+
+editInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    buttonToggle(
+      enableValidation(
+        editInputNombre,
+        editSpanNombre,
+        ValidationElements.edit.inputErrorClass
+      ),
+      enableValidation(
+        editInputAcerca,
+        editSpanAcerca,
+        ValidationElements.edit.inputErrorClass
+      ),
+      editButton,
+      ValidationElements.edit.inactiveButtonClass
+    );
+  });
+});
+
+addInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    buttonToggle(
+      enableValidation(
+        addInputTitulo,
+        addSpanTitulo,
+        ValidationElements.add.inputErrorClass
+      ),
+      enableValidation(
+        addInputImagen,
+        addSpanImagen,
+        ValidationElements.add.inputErrorClass
+      ),
+      addButton,
+      ValidationElements.add.inactiveButtonClass
+    );
+  });
+});
